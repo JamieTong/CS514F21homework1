2e4898768c219762063d34b864d7a19be144ba0f
Chris Brooks <chris.cbrooks@gmail.com>
2021-08-31 15:53:02 -0700

    Initial commit

---
+++ src/Fortune.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/** Please put your name, the class, and a short description of what Fortune does here. */
+
+
+public class Fortune {
+
+    public static Scanner getFileHandle(String filename) {
+
+    }
+
+
+    public static String getFirstFortune(Scanner fortuneFile) {
+
+
+    }
+
+    public static int fortuneCount(Scanner fortuneFile) {
+
+    }
+
+    public static String getRandomFortune(Scanner fortuneFile) {
+
+    }
+
+    public static void userInterface() {
+
+    }
+
+    public static void search(Scanner fortuneFile, String keyword) {
+
+    }
+
+    public static void writeFortune(String fortune, String filename) {
+
+    }
+
+    public static void shortFortunes(Scanner fortuneFile, int fortuneLength) {
+
+    }
+
+    public void test() {
+
+    }
+
+
+}
